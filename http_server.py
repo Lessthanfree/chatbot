ca95035066cdc459d44ac314a44557d0495f57b8
David Goh <gohzhiwei@hotmail.com>
2020-02-24 18:28:15 +0800

    Added xml formatter
    Replies with an xml just like the one sent by WeChat. This is automatically sent to the relevant user chat

--- http_server.py
+++ http_server.py
@@ -1,29 +1,18 @@
+import logging
+import time
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from http_utils import decode_post
 from urllib import parse
 # from chatbot import Chatbot
-import logging
+
+
+# ! NOTE ! http.server security is low
 
 # A server that recieves HTTP requests from the WeChat servers and breaks down the message to be fed into the chatbot.
 # Also sends messages back to the WeChat server to reply AND/OR sends a message to internal servers to trigger some action (log database info)
 
-# Converts "A=B&C=D" to {'A':B, 'C':D}
-def html_msg_to_dict(hmsg):
-    hmsg = "'" + hmsg
-    hmsg = hmsg.replace("&", "', '")
-    hmsg = hmsg.replace("=", "' :'")
-    hmsg = hmsg + "'"
-    hmsg_str = "{" + hmsg + "}"
-    c_dict = eval(hmsg_str)
-    print("CDICT", c_dict)
-    return c_dict
-
-def decode_post_json(data):
-    decoded_str = data.decode('utf-8')
-    print("Decoded post:", decoded_str)
-    data_d = eval(decoded_str)
-    print("IS dict:", isinstance(data_d, dict))
-    print("IS set:", isinstance(data_d, set))
-    return data_d
+ENCODING_USED = "utf-8"
 
 class ChatbotServer(BaseHTTPRequestHandler):
     def start_chatbot(self):
@@ -31,14 +20,30 @@ class ChatbotServer(BaseHTTPRequestHandler):
         print("disabled for now")
         # self.chatbot = Chatbot()
 
+    def format_reply_xml(self, msg_info, content):
+        reply = (
+            "<xml>"
+            "<ToUserName><![CDATA[%s]]></ToUserName>"
+            "<FromUserName><![CDATA[%s]]></FromUserName>"
+            "<CreateTime>%s</CreateTime>"
+            "<MsgType><![CDATA[text]]></MsgType>"
+            "<Content><![CDATA[%s]]></Content>"
+            "</xml>"
+        ) % (
+        msg_info['FromUserName'], 
+        msg_info['ToUserName'],
+        time.gmtime(),
+        content
+        )
+        return reply.encode(ENCODING_USED)
+
     def _get_bot_reply(self, info_dict):
         uid = info_dict.get("FromUserName", "")
         msg = info_dict.get("Content", "")
         logging.info("<SERVER GET BOT REPLY> USER <{}>:{}".format(uid, msg))
         # reply_obj = self.chatbot.get_bot_reply(uid, msg)
-        reply_text = "You said: <" + msg + ">"
+        reply_text = "You ({}) said: '{}'".format(uid, msg)
         reply_obj = (reply_text, {"A":50.0})
-        print("<SERVER> REPLY OBJ", reply_obj)
         return reply_obj
         
     def _set_response(self):
@@ -78,7 +83,7 @@ class ChatbotServer(BaseHTTPRequestHandler):
             echostr = req_dict.get("echostr")
             print("Sending auth code: {}".format(echostr))
             self._set_response()
-            auth_code = echostr.encode("utf-8")
+            auth_code = echostr.encode(ENCODING_USED)
             self.wfile.write(auth_code)
 
         req_dict = self._get_url_dict()
@@ -91,20 +96,23 @@ class ChatbotServer(BaseHTTPRequestHandler):
 
         
         self._set_response()
-        http_get_response = "GET request for {}".format(self.path).encode('utf-8')
+        http_get_response = "GET request for {}".format(self.path).encode(ENCODING_USED)
         logging.info("GET response:\n{}".format(http_get_response))
         self.wfile.write(http_get_response)
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        decoded = decode_post_json(post_data)
+        post_data_raw = self.rfile.read(content_length) # <--- Gets the data itself
+        request_info = decode_post(post_data_raw)
+
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), decoded)
+                str(self.path), str(self.headers), request_info)
 
         self._set_response()
-        chatbot_reply, breakdown = self._get_bot_reply(decoded)
-        self.wfile.write("POST REPLY: {}".format(chatbot_reply).encode('utf-8'))
+        chatbot_reply_str, breakdown = self._get_bot_reply(request_info)
+        post_reply = self.format_reply_xml(request_info, chatbot_reply_str)
+        logging.info("POST reponse:\n{}".format(chatbot_reply_str))
+        self.wfile.write(post_reply)
 
 def run(server_class=HTTPServer, handler_class=ChatbotServer, port=8080):
     logging.basicConfig(level=logging.INFO)
@@ -118,8 +126,6 @@ def run(server_class=HTTPServer, handler_class=ChatbotServer, port=8080):
         pass
     httpd.server_close()
     logging.info('Stopping httpd...\n')
-
-
 
 if __name__ == '__main__':
     from sys import argv
